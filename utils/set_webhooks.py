e78859604684ef6bda7565c46ce507b146e06b36
JoseMiracle <josephmiracle119@gmail.com>
2024-06-03 12:27:09 +0100

    fix:prod hitch

--- utils/set_webhooks.py
+++ utils/set_webhooks.py
@@ -10,7 +10,7 @@ if settings.DEBUG == True:
     WEBHOOK_URL = 'http://localhost:8000/bot/webhook/'
 
 else:
-    WEBHOOK_URL = os.getenv('PROD_URL_FOR_WEBHOOKS')+'bot/webhook/'
+    WEBHOOK_URL = os.getenv('PROD_URL_FOR_WEBHOOK')+'bot/webhook/'
 
 def set_webhook():
     url = f"https://api.telegram.org/bot{API_TOKEN}/setWebhook"
